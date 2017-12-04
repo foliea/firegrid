3af4449f483ee098666e44a798e1d6ed8158ef7a
Adrien Folie <folie.adrien@gmail.com>
2017-12-04 01:09:46 +0100

    add basic validations on configuration

---
+++ spec/config_spec.cr
@@ -0,0 +1,65 @@
+require "./spec_helper"
+require "../src/config"
+
+describe "Config" do
+  valid_body = %(
+    [colors]
+    border = "#000000"
+    font = "#ffffff"
+
+    [keys]
+    squares = ["a", "b", "c", "d"]
+  )
+
+  filename = "firegrid.toml"
+
+  describe "#validate!" do
+    context "with valid body" do
+      it "doesn't raise any exception" do
+        Config.new(valid_body, filename).validate!
+      end
+    end
+
+    context "when border color is not a valid hexadecimal color" do
+      it "raise an error" do
+        expect_raises(InvalidConfiguration) do
+          Config.new(valid_body.gsub("#000000", "000000"), filename).validate!
+        end
+      end
+    end
+
+    context "when font color is not a valid hexadecimal color" do
+      it "raise an error" do
+        expect_raises(InvalidConfiguration) do
+          Config.new(valid_body.gsub("#ffffff", "#fffff-"), filename).validate!
+        end
+      end
+    end
+
+    context "when square keys count is below the minimum grid size" do
+      it "raise an error" do
+        expect_raises(InvalidConfiguration) do
+          Config.new(valid_body.gsub(%(["a", "b", "c", "d"]), "[]"), filename).validate!
+        end
+      end
+    end
+  end
+
+  describe "#border_color" do
+    it "returns border color value from given body" do
+      Config.new(valid_body, filename).border_color.should eq("#000000")
+    end
+  end
+
+  describe "#font_color" do
+    it "returns font color value from given body" do
+      Config.new(valid_body, filename).font_color.should eq("#ffffff")
+    end
+  end
+
+  describe "#grid_size" do
+    it "equals square keys count from given body" do
+      Config.new(valid_body, filename).grid_size.should eq(4_u32)
+    end
+  end
+end
--- src/config.cr
+++ src/config.cr
@@ -8,15 +8,33 @@ class Config
     File.join("/etc", "firegrid", "firegrid.toml"),
   ]
 
+  # Try it here: http://rubular.com/r/cPuMa8WlKA
+  private HEXADECIMAL_COLOR_REGEXP = /^#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/
+
+  private MIN_GRID_SIZE = 4_u32
+
   def self.default : self
     FILENAMES.each do |filename|
-      return new(File.read(filename), filename) if File.exists?(filename)
+      next unless File.exists?(filename)
+
+      return new(File.read(filename), filename).tap { |config| config.validate! }
     end
+
     raise InvalidConfiguration.new("Configuration file not found! (#{FILENAMES})")
   end
 
   def initialize(body : String, @filename : String)
     @content = TOML.parse(body)
+  end
+
+  def validate!
+    validate_color!(border_color, key: "border")
+
+    validate_color!(font_color, key: "font")
+
+    if grid_size < MIN_GRID_SIZE
+      raise InvalidConfiguration.new("Please specify at least 4 square keys in #{@filename}")
+    end
   end
 
   def border_color
@@ -27,8 +45,18 @@ class Config
     @content["colors"].as(Hash)["font"].as(String)
   end
 
+  def grid_size
+    keys["squares"].size.to_u32
+  end
+
   def keybindings
     @_keybindings ||= Keybindings.new(keys)
+  end
+
+  private def validate_color!(value, key)
+    msg = "#{key.capitalize} color must be a valid hexadecimal color in #{@filename}"
+
+    raise InvalidConfiguration.new(msg) unless HEXADECIMAL_COLOR_REGEXP.match(value)
   end
 
   private def keys
