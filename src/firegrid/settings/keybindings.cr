684f44a892231625e9e6b52756c17942cf27fac0
Adrien Folie <folie.adrien@gmail.com>
2018-01-06 05:10:49 +0100

    refactor square keys in keybindings

--- src/firegrid/settings/keybindings.cr
+++ src/firegrid/settings/keybindings.cr
@@ -14,22 +14,26 @@ class Firegrid::Settings::Keybindings
   end
 
   def square_keycode?(keycode : String) : Bool
-    @keys["squares"].includes?(human_readable_key(keycode))
+    squares.includes?(human_readable_key(keycode))
   end
 
   def square_id(keycode : String) : Int32
     raise NoMatchingKey.new unless square_keycode?(keycode)
 
-    @keys["squares"].index(human_readable_key(keycode)).not_nil!
+    squares.index(human_readable_key(keycode)).not_nil!
   end
 
   def square_key(id : Int32) : String
-    raise NoMatchingKey.new unless (0..@keys["squares"].size).includes?(id)
+    raise NoMatchingKey.new unless (0..squares.size).includes?(id)
 
-    @keys["squares"][id].as(String)
+    squares[id].as(String)
   end
 
   private def human_readable_key(keycode : String) : String
     HUMAN_READABLE_KEYS.has_value?(keycode) ? HUMAN_READABLE_KEYS.key(keycode) : keycode
+  end
+
+  private def squares
+    @keys["squares"]
   end
 end
