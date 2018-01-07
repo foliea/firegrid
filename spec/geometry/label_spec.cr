c4cfc611caa5542978eaec985f9bd8d9802e4b2d
Adrien Folie <folie.adrien@gmail.com>
2018-01-07 21:15:16 +0100

    translate label origin depending on given content size

--- spec/geometry/label_spec.cr
+++ spec/geometry/label_spec.cr
@@ -4,14 +4,20 @@ describe "Label" do
   square = Square.new(50_u32, 50_u32, origin: Position.new(100_u32, 120_u32))
 
   describe "#size" do
-    it "returns the calculated size from given square" do
+    it "returns the calculated size depending on given square" do
       Label.new(square, "a").size.should eq(25_u32)
     end
   end
 
   describe "#origin" do
-    it "returns the calculated origin from given square" do
+    it "returns the calculated origin depending on given square" do
       Label.new(square, "a").origin.should eq(Position.new(113_u32, 157_u32))
+    end
+
+    context "when content size is above one character" do
+      it "translate its origin to the left depending on given content size" do
+        Label.new(square, "ab").origin.should eq(Position.new(101_u32, 157_u32))
+      end
     end
   end
 end
--- src/firegrid/geometry/label.cr
+++ src/firegrid/geometry/label.cr
@@ -4,13 +4,19 @@ require "./square"
 class Firegrid::Geometry::Label
   getter content : String
 
+  private SQUARE_HEIGHT_RATE = 0.5
+
   def initialize(@square : Square, @content : String); end
 
-  def size
-    (@square.width < @square.height ? @square.width : @square.height) / 2
+  def size : UInt32
+    (@square.height * SQUARE_HEIGHT_RATE).to_u32
   end
 
-  def origin
-    Position.new(@square.center.x - size / 2, @square.center.y + size / 2)
+  def origin : Position
+    Position.new(@square.center.x - half_size * @content.size, @square.center.y + half_size)
+  end
+
+  private def half_size
+    size / 2
   end
 end
